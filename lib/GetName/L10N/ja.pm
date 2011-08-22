32c73983be55830b238e44458459376fee740162
Alfasado Inc <webmaster@alfasado.jp>
2011-08-22 14:06:56 +0900

    first commit

---
+++ lib/GetName/L10N/ja.pm
@@ -0,0 +1,10 @@
+package GetName::L10N::ja;
+use strict;
+use base 'GetName::L10N';
+use vars qw( %Lexicon );
+
+our %Lexicon = (
+    'Get object name or label or title form id.' => 'IDからオブジェクトの名前を取得します。',
+);
+
+1;
---
+++ lib/GetName/Tags.pm
@@ -0,0 +1,23 @@
+package GetName::Tags;
+
+use strict;
+
+sub _hdlr_get_name {
+    my ( $ctx, $args, $cond ) = @_;
+    my $model = $args->{ model };
+    my $id = $args->{ id };
+    return '' unless $id;
+    return '' if ( $model eq 'author' );
+    return '' if ( $model eq 'config' );
+    my $obj = MT->model( $model )->load( $id );
+    return '' unless $obj;
+    if ( $obj->has_column( 'name' ) ) {
+        return $obj->name;
+    } elsif ( $obj->has_column( 'label' ) ) {
+        return $obj->label;
+    } elsif ( $obj->has_column( 'title' ) ) {
+        return $obj->title;
+    }
+}
+
+1;
